709ac0ac26c267fb9013520dffb376c97933a9e2
Cernous <redtraitor9@gmail.com>
2025-10-11 10:46:57 -0400

    fix: user model without the list of events saved or tickets

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,32 +7,31 @@ import uuid
 from pydantic import EmailStr
 from sqlmodel import Field, Relationship, SQLModel
 
+from typing import Literal
+
 class UserBase(SQLModel):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     email: EmailStr = Field(unique=True, index=True, max_length=255)
-    first_name: str = ""
-    last_name: str = ""
-    pronoun: str = ""
-    role: str = ""
-    username: str = ""
-    # password: str
-    # token: str
-    date_of_birth: str = ""
-    # saved_events: list[str]
-    # tickets: list[str]
+    first_name: str | None
+    last_name: str | None 
+    pronouns: str | None
+    username: str 
+    role: Literal['admin', 'organizer', 'student'] = "student"
 
-class UserCreate(SQLModel):
-    email: EmailStr = Field(unique=True, index=True, max_length=255)
-    date_of_birth: str  = ""
-    username: str = ""
-    first_name: str = ""
-    last_name: str = ""
-    password: str = ""
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=40)
 
-class UserSignIn(SQLModel):
-    username: str = ""
-    password: str = ""
+class UserUpdate(UserBase):
+    email: EmailStr | None = Field(unique=True, index=True, max_length=255)
+    password: str | None = Field(min_length=8, max_length=40)
 
-# class OrganizerCreate(UserCreate):
+class UserUpdatePassword(SQLModel):
+    current_password: str = Field(min_length=8, max_length=40)
+    new_password: str = Field(min_length=8, max_length=40)
+
+class User(UserBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid64, primary_key=True)
+    hashed_password: str
+
+
 
 
