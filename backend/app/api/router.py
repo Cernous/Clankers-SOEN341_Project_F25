af90dab9bae554ae46047f489a93945ee2bfb361
Cernous <redtraitor9@gmail.com>
2025-10-12 13:44:43 -0400

    unlocked api route for events

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -10,7 +10,7 @@ from core.config import settings
 
 api_router = APIRouter()
 # api_router.include_router(path.router) # this is how you add api endpoint paths from the routes folder
-# api_router.include_router(events.router)
+api_router.include_router(events.router)
 api_router.include_router(login.router)
 # api_router.include_router(users.router)
 # api_router.include_router(tools.router)
