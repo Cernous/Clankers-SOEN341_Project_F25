67d3f658dc7107c641f3efaee762d987f6953ac4
Cernous <redtraitor9@gmail.com>
2025-11-21 16:08:47 -0500

    fix: seed routes to work on appending instead of overwriting

--- backend/app/api/routes/seed_routes.py
+++ backend/app/api/routes/seed_routes.py
@@ -3,7 +3,7 @@ from sqlmodel import Session, func, select
 from datetime import datetime, timedelta, date, timezone
 import uuid
 
-from models import UserCreate, User, EventDB, UserRole
+from models import EventAdminCreate, UserCreate, User, EventDB, UserRole
 from api.deps import SessionDep
 import crud
 
@@ -59,63 +59,67 @@ def seed_database(session: SessionDep):
     ]
 
     for u in users:
-        user_sesh = session.exec(select(User).where(u.email == User.email or u.username == User.username)).all()
+        user_sesh = crud.verify_unique_email_username(session=session, username=u.username, email=u.email)
         if not user_sesh:
             crud.create_user(session=session, user_create=u)
     
+    uids = crud.get_uid_by_role(session=session, 
+                         role=UserRole.ORGANIZER)
+
     #event dummy data
     now = datetime.now(timezone.utc)
     events = [
-        EventDB(
+        EventAdminCreate(
             name="Tech Expo 2025",
             description="A showcase of student tech projects.",
             price=10.0,
             location="Montreal Convention Centre",
             start_time=now + timedelta(days=7),
             end_time=now + timedelta(days=7, hours=4),
-            organizer_id=3,
             tags="tech,expo,student",
             visibility="public",
-	    capacity=400,
+	        capacity=400,
             state="upcoming",
             count_attendees=0,
             tickets_left=50,
+            organizer_id=uids[0]
+            
         ),
-        EventDB(
+        EventAdminCreate(
             name="Cybersecurity Summit",
             description="Talks and demos on network security and hacking.",
             price=15.0,
             location="McGill Engineering Building",
             start_time=now + timedelta(days=14),
             end_time=now + timedelta(days=14, hours=6),
-            organizer_id=3,
             tags="cybersecurity,hacking,infosec",
             visibility="public",
-	    capacity=40,
+	        capacity=40,
             state="upcoming",
             count_attendees=0,
             tickets_left=30,
+            organizer_id=uids[0]
         ),
-        EventDB(
+        EventAdminCreate(
             name="AI and Ethics Forum",
             description="Panel discussion on ethical AI deployment.",
             price=0.0,
             location="Concordia Hall",
             start_time=now + timedelta(days=21),
             end_time=now + timedelta(days=21, hours=3),
-            organizer_id=3,
             tags="AI,ethics,forum",
             visibility="public",
-	    capacity=80,
+	        capacity=80,
             state="upcoming",
             count_attendees=0,
             tickets_left=100,
+            organizer_id=uids[0]
         ),
     ]
 
     
     for e in events:
-        session.add(e)
+        session.add(EventDB(**e.model_dump()))
 
     session.commit()
     return {"message": "Dummy users and events successfully seeded."}
