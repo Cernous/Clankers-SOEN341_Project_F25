9ad85261f46a4e64932c4f9c691564ca27f35e92
Batu <b_erata@live.concordia.ca>
2025-11-23 18:42:29 -0800

    nothing changed testing eslint 3

--- frontend/app/src/router.tsx
+++ frontend/app/src/router.tsx
@@ -1,7 +1,7 @@
 import { createRouter as createTanstackRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen.js'
+import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
 export const createRouter = () => {
